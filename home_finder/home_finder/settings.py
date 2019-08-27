285a30713e92b63cf97ae8771af62d2a74755d78
nekomamoushi <germione.grangere@gmail.com>
2019-08-27 11:26:56 +0200

    feat: setup scrapy-splash for generated html page

--- home_finder/home_finder/settings.py
+++ home_finder/home_finder/settings.py
@@ -14,6 +14,23 @@ BOT_NAME = 'home_finder'
 SPIDER_MODULES = ['home_finder.spiders']
 NEWSPIDER_MODULE = 'home_finder.spiders'
 
+# Scrapy Splash
+SPLASH_URL = 'http://localhost:8050'
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'home_finder (+http://www.yourdomain.com)'
 USER_AGENT_LIST = [
@@ -28,7 +45,7 @@ USER_AGENT_LIST = [
 ]
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- home_finder/home_finder/spiders/seloger.py
+++ home_finder/home_finder/spiders/seloger.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy import Spider
+
+from scrapy_splash import SplashRequest
 
 
-class SelogerSpider(scrapy.Spider):
+
+class SelogerSpider(Spider):
     name = 'seloger'
     allowed_domains = ['seloger.com']
     start_urls = ['https://www.seloger.com/']
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield SplashRequest(url, callback=self.parse,args={'wait':'5'})
 
     def parse(self, response):
         pass
