a1b66b17a75e8b29661d6e4162e2cd2c7698365a
nekomamoushi <germione.grangere@gmail.com>
2019-08-27 12:00:03 +0200

    fix: read project settings

--- home_finder/run.py
+++ home_finder/run.py
@@ -2,18 +2,24 @@
 import os
 
 from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
 from scrapy.utils.project import get_project_settings
 
 from home_finder.spiders.seloger import SelogerSpider
 from home_finder.utils import retrieve_environ
 
 
-settings = get_project_settings()
-dropbox_token = retrieve_environ("DROPBOX_TOKEN")
-settings.set("DROPBOX_TOKEN", dropbox_token)
-print(list(settings.keys()))
-exit()
-crawler = CrawlerProcess(settings=settings)
+def setup_settings():
+    settings = Settings()
+    settings_module_path = 'home_finder.settings'
+    settings.setmodule(settings_module_path, priority='project')
+    dropbox_token = retrieve_environ("DROPBOX_TOKEN")
+    settings.set("DROPBOX_TOKEN", dropbox_token)
+    return settings
+
+home_finder_settings = settings=setup_settings()
+
+crawler = CrawlerProcess(settings=home_finder_settings)
 
 crawler.crawl(SelogerSpider)
 crawler.start()
