f612fbbc742813b41bc26afe1ca45f49a38d5742
nekomamoushi <germione.grangere@gmail.com>
2019-08-28 17:00:55 +0200

    fix: change import and settings due to folder name change

--- home_finder/crawler/settings.py
+++ home_finder/crawler/settings.py
@@ -13,8 +13,8 @@ from shutil import which
 
 BOT_NAME = 'home_finder'
 
-SPIDER_MODULES = ['home_finder.spiders']
-NEWSPIDER_MODULE = 'home_finder.spiders'
+SPIDER_MODULES = ['crawler.spiders']
+NEWSPIDER_MODULE = 'crawler.spiders'
 
 # Scrapy Selenium
 SELENIUM_DRIVER_NAME = 'firefox'
@@ -64,7 +64,7 @@ COOKIES_ENABLED = False
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 SPIDER_MIDDLEWARES = {
-    'home_finder.middlewares.RotateUserAgentMiddleware': 543,
+    'crawler.middlewares.RotateUserAgentMiddleware': 543,
 }
 
 # Enable or disable downloader middlewares
@@ -85,7 +85,7 @@ LOG_LEVEL = 'INFO'
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'home_finder.pipelines.HomeFinderPipeline': 300,
+    'crawler.pipelines.HomeFinderPipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
--- home_finder/run.py
+++ home_finder/run.py
@@ -5,13 +5,13 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 from scrapy.utils.project import get_project_settings
 
-from home_finder.spiders.seloger import SelogerSpider
-from home_finder.utils import retrieve_environ
+from crawler.spiders.seloger import SelogerSpider
+from crawler.utils import retrieve_environ
 
 
 def setup_settings():
     settings = Settings()
-    settings_module_path = 'home_finder.settings'
+    settings_module_path = 'crawler.settings'
     settings.setmodule(settings_module_path, priority='project')
     settings.set("DROPBOX_TOKEN", retrieve_environ("DROPBOX_TOKEN"))
     settings.set("DROPBOX_SETTINGS_FILENAME", "/home-finder/settings.yml")
